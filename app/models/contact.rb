d052a90af24bd48be771127a6c801e58e72f830b
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-09-28 12:38:41 +0530

    Validation added

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 class Contact < ApplicationRecord
     validates :first_name,presence: true 
     validates :phone_number,presence: true, length:{is: 10 }
+    validates_uniqueness_of :phone_number
+    validates_uniqueness_of :email
     has_one_attached :image 
     paginates_per 6
 end
