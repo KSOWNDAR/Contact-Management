c4e3dedb76df018c350dc0cce8b88e3fe2311e42
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-09-28 12:10:52 +0530

    Faker created

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ contacts = []
     id: i,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    phone_number: 9789655333,
+    phone_number: Faker::Number.number(digits:10),
     email: Faker::Internet.email,
     company_name: Faker::Company.name,
     birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
